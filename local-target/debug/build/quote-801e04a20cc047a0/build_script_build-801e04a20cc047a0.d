22c28c22a65377f3bf1c7698617bec9ff218b18e
Jean-Charles GOULEAU <jean-charles.gouleau@savoye.com>
2025-11-12 17:19:48 +0100

    chore: ignore local target artifacts

--- local-target/debug/build/quote-801e04a20cc047a0/build_script_build-801e04a20cc047a0.d
+++
@@ -1,5 +0,0 @@
-/home/jcgouleau/IdeaProjects/RustProject/chemins_noirs/./local-target/debug/build/quote-801e04a20cc047a0/build_script_build-801e04a20cc047a0.d: /home/jcgouleau/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.42/build.rs
-
-/home/jcgouleau/IdeaProjects/RustProject/chemins_noirs/./local-target/debug/build/quote-801e04a20cc047a0/build_script_build-801e04a20cc047a0: /home/jcgouleau/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.42/build.rs
-
-/home/jcgouleau/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.42/build.rs:
--- local-target/debug/deps/itoa-f2b51776301c1a6f.d
+++
@@ -1,8 +0,0 @@
-/home/jcgouleau/IdeaProjects/RustProject/chemins_noirs/./local-target/debug/deps/itoa-f2b51776301c1a6f.d: /home/jcgouleau/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /home/jcgouleau/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
-
-/home/jcgouleau/IdeaProjects/RustProject/chemins_noirs/./local-target/debug/deps/libitoa-f2b51776301c1a6f.rlib: /home/jcgouleau/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /home/jcgouleau/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
-
-/home/jcgouleau/IdeaProjects/RustProject/chemins_noirs/./local-target/debug/deps/libitoa-f2b51776301c1a6f.rmeta: /home/jcgouleau/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /home/jcgouleau/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
-
-/home/jcgouleau/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs:
-/home/jcgouleau/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs:
